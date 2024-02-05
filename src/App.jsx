b8501bb9cc38809d7f1b4deba89de8d25d6d4b04
unknown <tarakktari2003@gmail.com>
2024-02-05 13:04:30 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import * as React from "react";
+const welcome={
+  greeting:"hey",
+  title:"react",
+};
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1> {welcome.greeting}{welcome.title}</h1>
+      <label htmlFor="search">search:</label>
+      <input id="search" type="next"/>
 
-export default App
+    </div>
+    
+  );
+}
+export default App;
