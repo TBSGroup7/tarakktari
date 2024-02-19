3aa429e53c2c773f7e58d1f82f7943e2ebb9361e
TarakKtari <tarakktari2003@gmail.com>
2024-02-19 11:49:11 +0100

    W4 React Multiple Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,59 @@
 import * as React from "react";
-const welcome={
-  greeting:"hey",
-  title:"react",
-};
-function App() {
-  return (
-    <div>
-      <h1> {welcome.greeting}{welcome.title}</h1>
-      <label htmlFor="search">search:</label>
-      <input id="search" type="next"/>
 
-    </div>
-    
-  );
-}
-export default App;
+const list = [
+  {
+  title: "React",
+  url: "https://reactjs.org/", author: "Jordan Walke",
+  num_comments: 3,
+  points: 4,
+  objectID: 0,
+  },
+  {
+  title: "Redux",
+  url: "https://redux.js.org/",
+  author: "Dan Abramov, Andrew Clark",
+  num_comments: 2,
+  points: 5,
+  objectID: 1,
+},
+];
+
+function App() { 
+  return ( 
+  <div> 
+  <h1>My Hacker Stories</h1> 
+  <Search /> 
+  <hr /> 
+  <List /> 
+  </div> 
+  ); 
+  } 
+
+function List() { 
+  return ( 
+    <ul> 
+      {list.map(function (item) { 
+        return ( 
+          <li key={item.objectID}> 
+            <span> 
+              <a href={item.url}>{item.title}</a> 
+            </span> 
+            <span>{item.author}</span> 
+            <span>{item.num_comments}</span> 
+            <span>{item.points}</span> 
+          </li> 
+        ); 
+      })} 
+    </ul> 
+  ); 
+} 
+
+function Search() { 
+  return ( 
+    <div> 
+      <label htmlFor="search">Search: </label> 
+      <input id="search" type="text" /> 
+    </div> 
+  ); 
+} 
+export default App
